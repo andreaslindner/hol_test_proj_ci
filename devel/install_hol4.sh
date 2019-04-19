4f04fecbae38d14dc4fe0d4730ef778f9956a4fa
Andreas Lindner <andreas.lindner@a-eye.se>
2019-04-19 17:22:21 +0200

    first travis ci trial with HOL4 dependency

---
+++ devel/install_hol4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# exit immediately if an error happens
+set -e
+
+# go to cache directory
+mkdir -p ${CACHE_DIR}
+cd ${CACHE_DIR}
+
+# clone the right HOL4 branch
+git clone -b for_holba --single-branch git@github.com:kth-step/HOL.git
+cd HOL
+
+# compile HOL4
+poly < tools/smart-configure.sml
+bin/build --nograph
+
---
+++ devel/install_poly.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# exit immediately if an error happens
+set -e
+
+# go to cache directory
+mkdir -p ${CACHE_DIR}
+cd ${CACHE_DIR}
+
+
+# based on HOL4 developers/install-poly.sh
+# --------------------------------------------
+POLY_BASE="https://github.com/polyml/polyml"
+POLY_VERSION=${POLY_VERSION:-v5.7.1}
+
+PREFIX=${CACHE_DIR}/polyml
+
+
+# prepare directories
+mkdir polyml_src
+mkdir polyml
+
+# download polyml
+wget -qO- ${POLY_BASE}/archive/${POLY_VERSION}.tar.gz | \
+  tar xvz -C polyml_src --strip-components 1
+
+# compile polyml
+cd polyml_src
+echo "*** Configuring PolyML for prefix: ${PREFIX}"
+./configure --prefix=$PREFIX
+make
+
+# install polyml
+make install
+
