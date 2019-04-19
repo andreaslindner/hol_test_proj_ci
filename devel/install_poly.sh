bfc900a76a73bed063ef925f438320cea90483cb
Andreas Lindner <andreas.lindner@a-eye.se>
2019-04-19 20:09:59 +0200

    fixing caching and compilation error in src

--- devel/install_poly.sh
+++ devel/install_poly.sh
@@ -14,10 +14,12 @@ cd ${CACHE_DIR}
 # based on HOL4 developers/install-poly.sh
 # --------------------------------------------
 POLY_BASE="https://github.com/polyml/polyml"
-POLY_VERSION=${POLY_VERSION:-v5.7.1}
+POLY_VERSION="v5.7.1"
+POLY_URL=${POLY_BASE}/archive/${POLY_VERSION}.tar.gz
 
 PREFIX=${CACHE_DIR}/polyml
 
+# if the output directory exists, we already have a polyml in the cache
 if [ -d "$PREFIX" ]; then
   echo "polyml is already available in the cache, exiting."
   exit 0
@@ -28,7 +30,7 @@ mkdir polyml_src
 mkdir polyml
 
 # download polyml
-wget -qO- ${POLY_BASE}/archive/${POLY_VERSION}.tar.gz | \
+wget -qO- ${POLY_URL} | \
   tar xvz -C polyml_src --strip-components 1
 
 # compile polyml
